b951fd123deee4b1521eb9138e819d18185ccb5f
adypr <workady2015@gmail.com>
2023-10-30 02:40:37 +0300

    feat: add components

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ErrorBoundary;
---
+++ src/components/Search/index.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import './Search.scss';
+
+class Search extends Component<unknown, { value: string }> {
+  constructor(props: unknown) {
+    super(props);
+    this.state = {
+      value: localStorage.getItem('searchValue') || '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  componentWillUnmount(): void {
+    localStorage.setItem('searchValue', this.state.value);
+  }
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className="search">
+        <label>
+          Search
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={value}
+            placeholder="search..."
+          />
+        </label>
+        <button className="search__button">Find item</button>
+      </div>
+    );
+  }
+}
+
+export default Search;
